518e687bedbef67b16b24a58e27851aa33740356
Trenersambo <74851585+trenersambo@users.noreply.github.com>
2022-10-18 23:29:12 +0300

    Домашнее №2 по C# (
    Семинар был 17.10.2022.

---
+++ Task13/ShowThird.cs
@@ -0,0 +1,36 @@
+﻿  /*
+  Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+  или сообщает, что третьей цифры нет.
+
+  645 -> 5
+  78 -> третьей цифры нет
+  32679 -> 6
+  */
+ 
+void ShowThird(int num){
+
+  // для работы с индексами числа
+  string numStr = num.ToString() ;   
+  int numLength = numStr.Length ;  
+ 
+
+  if(numLength <=2){
+
+    Console.WriteLine($"No third number");  
+
+  }else if(numLength >2){
+
+    Console.WriteLine($" This is the third number: {numStr[2]} from {num}") ;
+
+  } ;
+
+}
+
+
+Console.WriteLine("Enter a number greater than zero:") ;  
+int num = Convert.ToInt32(Console.ReadLine() ) ; 
+
+ShowThird(num);
+
+
+
---
+++ Task15/DayOff.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+  bool showDay(int day){
+ 
+  if(day ==6 || day ==7){
+
+    return true ;
+  
+  }else {
+
+    return false;
+
+  }
+
+} ;
+
+ 
+Console.WriteLine("Write the day of the week: " ) ;  
+int day = Convert.ToInt32(Console.ReadLine() ) ; 
+
+ 
+bool dayOff = showDay(day) ;
+
+if(dayOff){
+  Console.WriteLine($" {day} day of the week - relax day");
+}else{
+  Console.WriteLine($" {day} day of the week - Go & working");
+}
